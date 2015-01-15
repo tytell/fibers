7f35f76a66e93d78dc0dfd98c5f1e8953e630f4f
Eric Tytell <Eric.Tytell@tufts.edu>
2015-01-15 13:28:28 -0500

    Updates to work on HDF5 files

--- fiberAngle3.m
+++ fiberAngle3.m
@@ -1,4 +1,4 @@
-function varargout = fiberAngle3(x,y,z, V, grid, corrfile, dbg)
+function fiberAngle3(h5imagefile,h5outfile, igrid, varargin)
 %function [x,y,z,u,v,w, eigval] = fiberAngle3(x,y,z, V, grid, corrfile, dbg)
 %x,y are by pixel coordinates (i.e., not just the endpoints)
 %z is by slice coordinates
@@ -11,84 +11,77 @@ function varargout = fiberAngle3(x,y,z, V, grid, corrfile, dbg)
 %(optional) dbg is true or false for debugging.  With debugging on, does not
 %actually perform the correlations
 
-%sort out parameters
-if (nargin < 7),
-  dbg = 0;
-  if (nargin < 6),
-    corrfile = [];
-  end;
-end;
+opt.savecorr = false;
+opt.saveeig = false;
+opt.debug = false;
+opt = parsevarargin(opt,varargin,3);
 
-%check whether we need to save the correlations
-if (isempty(corrfile)),
-  saveCorr = false;
-else
-  saveCorr = true;
-end;
+info = h5info(h5imagefile,'/Image');
+sz = info.Dataspace.Size;
 
-%same for the eigenvalues
-if (nargout == 7),
-    saveEig = true;
+attrnames = {info.Attributes.Name};
+if ismember('Scale',attrnames)
+    scale = h5readatt(h5imagefile,'/Image','Scale');
 else
-    saveEig = false;
-end;
+    scale = 1;
+end
+if ismember('Maximum',attrnames)
+    imagemax = h5readatt(h5imagefile,'/Image','Maximum');
+    imagemax = double(imagemax);
+else
+    imagemax = 1;
+end
 
-%check the data type of the image
-if (~strcmp(class(V),'double')),
-    convertToDouble = true;
-    maxIntensity = double(max(V(:)));
-else
-    convertToDouble = false;
-end;
+if numel(scale) == 1
+    scale = scale*[1 1 1];
+end
 
-%set up an inverse scale (world units to pixels)
-if (length(z) > 1),
-  scale = 1./abs([x(2)-x(1) y(2)-y(1) z(2)-z(1)]);
-else
-  scale = 1./abs([x(2)-x(1) y(2)-y(1)]);
-  scale(3) = 0;
-end;
+%note that x axis is the first dimension in the data set, not the second
+x = ((1:sz(1)) - (sz(1)+1)/2) * scale(1);
+y = ((1:sz(2)) - (sz(2)+1)/2) * scale(2);
+z = ((1:sz(3)) - (sz(3)+1)/2) * scale(3);
 
 %get the limits of the volume
 lim = [min(x) min(y) min(z); max(x) max(y) max(z)];
-%and the number of pixels
-sz = size(V);
 
 %can directly specify the position of the correlation volumes, if you
 %want, using a cell array for grid
-if (iscell(grid)),
-    ctrx = grid{1};
-    ctry = grid{2};
-    ctrz = grid{3};
+if (iscell(igrid)),
+    ictr1 = igrid{1};
+    ictr2 = igrid{2};
+    ictr3 = igrid{3};
 
-    ictr1 = round((ctrx-lim(1,1))*scale(1)) + 1;
-    ictr2 = round((ctry-lim(1,2))*scale(2)) + 1;
-    ictr3 = round((ctrz-lim(1,3))*scale(3)) + 1;
+    ctrx = round((ictr1-1)*scale(1)) + lim(1);
+    ctry = round((ictr2-1)*scale(2)) + lim(2);
+    ctrz = round((ictr3-1)*scale(3)) + lim(3);
 
-    igrid = round(grid{4}.*scale);
+    igrid = igrid{4};
+    iscenter = true;
 else
     %otherwise, do it the normal way
     %grid can also specify non-square regions if it's six elements long,
     %with the first 3 elements being the size in x y and z and the second
     %3 elements being the step in x y and z
-    if (length(grid) == 2),
-        grid = grid([1 1 1 2 2 2]);
+    if (length(igrid) == 2),
+        igrid = igrid([1 1 1 2 2 2]);
     end;
 
-    %get the grid size in pixels
-    igrid = round(grid(1:3).*scale);
+    %get the grid size in real units
+    vgrid = igrid.*[scale scale];
     %can't have a grid larger than the volume
     if (any(igrid(1:3) > sz(1:3))),
         warning('Grid sizes are larger than the volume.  Adjusting down.');
         k = find(igrid(1:3) > sz(1:3));
         igrid(k) = sz(k);
     end;
+    iscenter = false;
 end;
 
 %set up the coordinates for each correlation volume
 %centers of the volumes in x y and z
 %the "i" prefix is for an index/pixel coordinate
 irgnctr = floor(igrid/2);
+nrgn = 2*irgnctr(1:3);
 
 %pixel coordinates (irgn1) and real coordinates (rgnx)
 irgn1 = -(irgnctr(1)-1):irgnctr(1);
@@ -98,43 +91,24 @@ irgn2 = -(irgnctr(2)-1):irgnctr(2);
 rgny = irgn2/scale(2);
 
 irgn3 = -(irgnctr(3)-1):irgnctr(3);
-rgnz0 = irgn3/scale(3);
+rgnz = irgn3/scale(3);
 
 %generate the centers of the correlation volumes so that they're centered
 %within the entire volume
-if (~iscell(grid)),
-    igridstep = round(grid(4:6).*scale);
+if (~iscenter),
+    igridstep = igrid(4:6);
 
     ictr1 = round(irgnctr(1)+1:igridstep(1):sz(2)-irgnctr(1));
     ictr1 = ictr1 + round((sz(2)-irgnctr(1)-ictr1(end))/2);
     ictr2 = round(irgnctr(2)+1:igridstep(2):sz(1)-irgnctr(2));
     ictr2 = ictr2 + round((sz(1)-irgnctr(2)-ictr2(end))/2);
     ictr3 = round(irgnctr(3)+1:igridstep(3):sz(3)-irgnctr(3));
-    %make sure we have at least *some* points in the z direction
-    if (isempty(ictr3)),
-        ictr3 = ceil(sz(3)/2);
-        k = find((ictr3 + irgn3 >= 1) & (ictr3 + irgn3 <= sz(3)));
-        irgn3 = irgn3(k);
-        rgnz0 = rgnz0(k);
-    end;
     ictr3 = ictr3 + round((sz(3)-irgnctr(3)-ictr3(end))/2);
 
     %get the real coordinates
-    ctrx = (ictr1-1)/scale(1) + lim(1,1);
-    ctry = (ictr2-1)/scale(2) + lim(1,2);
-    ctrz = (ictr3-1)/scale(3) + lim(1,3);
-end;
-
-%scale up the z dimension so that its scale matches the x and y
-%dimensions.  Normally z is much coarser than x and y, so we'll have to
-%interpolate, but it's important to make the FFT work reasonably
-if (scale(3) < scale(1)),
-    sc = scale(1);
-    
-    irgnctr(3) = floor(-rgnz0(1).*sc)+1;
-    rgnz = (-(irgnctr(3)-1):irgnctr(3))/sc;
-else
-    rgnz = rgnz0;
+    ctrx = (ictr1-1)*scale(1) + lim(1,1);
+    ctry = (ictr2-1)*scale(2) + lim(1,2);
+    ctrz = (ictr3-1)*scale(3) + lim(1,3);
 end;
 
 % h = (-3:3)';
@@ -143,8 +117,8 @@ difford = 5;
 hctr = ceil(length(rgnx)/2);
 
 %set up the windowing transform (kaiser)
-w1 = kaiser(length(rgny),0.5);
-w2 = kaiser(length(rgnx),0.5);
+w1 = kaiser(length(rgnx),0.5);
+w2 = kaiser(length(rgny),0.5);
 w3 = kaiser(length(rgnz),0.5);
 wind = repmat(w1,[1 length(w2) length(w3)]) .* ...
     repmat(w2',[length(w1) 1 length(w3)]) .* ...
@@ -153,30 +127,42 @@ wind = repmat(w1,[1 length(w2) length(w3)]) .* ...
 diff1 = [];
 diff2 = [];
 
-if (saveCorr),
-    corrfid = fopen(corrfile,'wb');
-    fwrite(corrfid,[length(ictr1) length(ictr2) length(ictr3)],'int8');
-    fwrite(corrfid,[length(rgnx) length(rgny) length(rgnz)],'int8');
+if (opt.savecorr)
+    h5opencreate(h5outfile,'/Correlations',[length(rgnx) length(rgny) length(rgnz) ...
+        length(ictr1) length(ictr2) length(ictr3)], ...
+        'ChunkSize',[length(rgnx) length(rgny) length(rgnz) 1 1 1]);
+    h5opencreate(h5outfile,'/Hessians',[3 3 ...
+        length(ictr1) length(ictr2) length(ictr3)], ...
+        'ChunkSize',[3 3 1 1 1]);
 end;
-    
+if (opt.saveeig)
+    h5opencreate(h5outfile,'/Eigenvalues',[3 ...
+        length(ictr1) length(ictr2) length(ictr3)], ...
+        'ChunkSize',[3 1 1 1]);
+end    
+h5opencreate(h5outfile,'/Eigenvectors',[3 3 ...
+    length(ictr1) length(ictr2) length(ictr3)], ...
+    'ChunkSize',[3 3 1 1 1]);
+h5opencreate(h5outfile,'/Grid/X', size(ctrx));
+h5write(h5outfile,'/Grid/X', ctrx);
+h5opencreate(h5outfile,'/Grid/Y', size(ctry));
+h5write(h5outfile,'/Grid/Y', ctry);
+h5opencreate(h5outfile,'/Grid/Z', size(ctrz));
+h5write(h5outfile,'/Grid/Z', ctrz);
+h5writeatt(h5outfile,'/Grid','CorrelationVolume',vgrid(1:3));
+
 timedWaitBar(0, 'Fiber angles...');
 a = 1;
 N = length(ictr1)*length(ictr2)*length(ictr3);
 for i = 1:length(ictr1),
     for j = 1:length(ictr2),
         for k = 1:length(ictr3),
-            V1 = V(ictr2(j)+irgn2, ictr1(i)+irgn1, ictr3(k)+irgn3);
-            if (convertToDouble),
-                V1 = double(V1)/maxIntensity;
-            end;
-
+            V1 = h5read(h5imagefile,'/Image',[ictr1(i)-irgnctr(1) ictr2(j)-irgnctr(2) ictr3(k)-irgnctr(3)], ...
+                nrgn);
+            V1 = double(V1) / imagemax;
+            
             %check to make sure that there's a signal in this region
-            if (~dbg & (std(V1(:)) ~= 0))
-                %interpolate in z if necessary
-                if (length(rgnz) > length(rgnz0)),
-                    V1 = interp3(rgnx,rgny,rgnz0,V1, rgnx,rgny',rgnz);
-                end;
-
+            if (~opt.debug && (rms(V1(:)) > 0.05))
                 %apply the window
                 V1 = V1.*wind;
 
@@ -225,45 +211,24 @@ for i = 1:length(ictr1),
                 [m,ind] = min(abs(ed));
 
                 %save correlation matrix and/or eigenvalues
-                if (saveCorr),
-                    fwrite(corrfid,C,'double');
-                    fwrite(corrfid,H,'double');
+                if (opt.savecorr),
+                    h5write(h5outfile,'/Correlations',C,[1 1 1 j i k],...
+                        [size(C) 1 1 1]);
+                    h5write(h5outfile,'/Hessians',H,[1 1 j i k],...
+                        [size(H) 1 1 1]);
                 end;
-                if (saveEig),
-                    eigval(:,j,i,k) = ed([ind 1:ind-1 ind+1:end]);
-                end;
-
-                %save the eigenvector
-                u(j,i,k) = ev(1,ind);
-                v(j,i,k) = ev(2,ind);
-                w(j,i,k) = ev(3,ind);
-            else
-                u(j,i,k) = 0;
-                v(j,i,k) = 0;
-                w(j,i,k) = 0;
+                if (opt.saveeig),
+                    h5write(h5outfile,'/Eigenvalues',ed([ind 1:ind-1 ind+1:end]),[1 j i k],...
+                        [3 1 1 1]);
+                end
+                h5write(h5outfile,'/Eigenvectors',ev(:,[ind 1:ind-1 ind+1:end]),[1 1 j i k],...
+                    [3 3 1 1 1]);
             end;
             if (~timedWaitBar(a/N)),
-                if (saveCorr),
-                    fclose(fid);
-                end;
-                varargout = {ctrx,ctry,ctrz, u,v,w};
                 return;
             end;
             a = a+1;
         end;
     end;
 end;
-
-if (saveCorr),
-    fclose(corrfid);
-end;
-if (dbg),
-    varargout = cell(1,nargout);
-else
-    varargout = {ctrx,ctry,ctrz, u,v,w};
-    if (saveEig),
-        varargout(7) = {eigval};
-    end;
-end;
-
 
